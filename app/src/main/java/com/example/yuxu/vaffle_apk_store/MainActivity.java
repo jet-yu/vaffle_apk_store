0d68acaaa0d9e6e3a58116c57a97ffbb45d87531
yuxu <woniuatyu@163.com>
2019-01-30 13:56:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yuxu/vaffle_apk_store/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yuxu.vaffle_apk_store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
